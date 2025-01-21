379a6906a9b65924db1f5a97801fd908fe59a66e
vosidovmsaid2007 <msaidvosidov@gmail.com>
2025-01-21 22:46:09 +0500

    Finished project

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel(){
+        completeLevelUI.SetActive(true);
+    }
+
     public void EndGame(){
-        Debug.Log("Game Over!");
+        if(gameHasEnded == false){
+            gameHasEnded = true;
+            Debug.Log("Game Over!");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void NextLevel(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitButton(){
+        #if UNITY_STANDALONE
+            Application.Quit();
+        #endif
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 
 
